f2db778e5d9c7b7cf1014180aa31f46a6860d4a0
DARKDEV <121659506+DARKDEV22@users.noreply.github.com>
2023-09-02 19:29:37 +0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,7 +4,7 @@ use std::thread;
 use std::time::Duration;
 use std::fs;
 use std::io::Cursor;
-
+use std::io;
 
 fn get_m3u8(user_id: &str) -> Result<String, Box<dyn std::error::Error>> {
 
@@ -12,7 +12,7 @@ fn get_m3u8(user_id: &str) -> Result<String, Box<dyn std::error::Error>> {
     headers.insert("authority", "www.bigo.tv".parse().unwrap());
     headers.insert("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8".parse().unwrap());
     headers.insert("accept-language", "en-US,en;q=0.6".parse().unwrap());
-    headers.insert("cache-control", "max-age=0".parse().unwrap());
+    // headers.insert("cache-control", "max-age=0".parse().unwrap());
     headers.insert("if-none-match", "\"2232d-VHn+zGI8e88vY+/Y2VFdd20rpsE\"".parse().unwrap());
     headers.insert("sec-ch-ua", "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Brave\";v=\"116\"".parse().unwrap());
     headers.insert("sec-ch-ua-mobile", "?1".parse().unwrap());
@@ -74,7 +74,7 @@ async fn download_file(download_url: &str, save_path: &str) -> Result<u64, Box<d
     let response = reqwest::get(download_url).await?;
     let file_size = response.content_length().unwrap_or(0);
     // if it found video not "not found" page
-    if file_size > 100 {
+    if file_size > 200 {
         let mut file = std::fs::File::create(save_path)?;
         let mut content = Cursor::new(response.bytes().await?);
         std::io::copy(&mut content, &mut file)?;
@@ -84,14 +84,16 @@ async fn download_file(download_url: &str, save_path: &str) -> Result<u64, Box<d
 
 #[tokio::main]
 async fn main() {
-    let user_id = "Numwarn12345678";
+    // let user_id = "Money_spis";
+    let mut user_id_input = String::new();
+    println!("\nEnter bigo user_id_input :");
+    io::stdin().read_line(&mut user_id_input).expect("Failed to read");
+    let user_id = user_id_input.trim();
+
     let delay_sec = 2;
     let sec_interval = Duration::from_millis(300);
     let retry_interval = Duration::from_millis(200);
-
-    // create directory
-    let _dir = create_dir(user_id);
-
+    
     match get_m3u8(user_id) {
         Ok(response) => {
             // get m3u8_url 
@@ -120,6 +122,8 @@ async fn main() {
             // m3u8 response -> ts4 download url
             match get_ts4(&m3u8_url){
                 Ok(response) => {
+                    // create directory
+                    let _dir = create_dir(user_id);
                     let r: Vec<&str> = response.split("\n").collect();
                     let base_id: Vec<&str> = r[2].split(":").collect();
                     if let Ok(start_id) = base_id[1].parse::<i32>() {
@@ -130,7 +134,8 @@ async fn main() {
                         // println!("{:?} {:?} {:?}", start_id, base_url, base_nums_vec);
 
                         // download section
-                        println!("Recording... {}", user_id);
+                        println!("\n[INFO] Ctrl + C : stop recording.");
+                        println!("\nRecording: {}", user_id);
                         let mut n_not_complete: u8 = 0;
                         let limit_download_attempt: u8 = 36;
                         let may_be_next_id: u8 = 12;
@@ -138,7 +143,10 @@ async fn main() {
                         while  n_not_complete <= limit_download_attempt {
                             let download_url = format!("{}/{}_{}&{}&0.ts", base_url, base_nums_vec[0], start_id, base_nums_vec[1]);
                             let save_path = format!("{}/{}.ts", user_id, start_id);
-                            println!("[{}] - {}", n_videos, start_id);
+                            println!("id: {}: {} | {}", user_id, start_id, n_videos);
+                            if n_videos%40 == 0 {
+                                println!(" -- estimate {:.2} minutes.", (n_videos*2) as f32/60.0);
+                            }
                             let result = download_file(&download_url, &save_path).await;
                             // check is valid file ?
                             let file_size = match result {
@@ -149,7 +157,7 @@ async fn main() {
                                 }
                             };
 
-                            if file_size < 100 {
+                            if file_size < 200 {
                                 n_not_complete += 1;
                                 thread::sleep(retry_interval);
                                 if n_not_complete > may_be_next_id {
@@ -170,11 +178,10 @@ async fn main() {
                         println!("Conversion failed");
                     }
                 },
-                Err(err) => {
-                    eprintln!("Error: {}", err);
+                Err(_) => {
+                    println!("\nUser id: {} not found", user_id);
                 }
-            }
-              
+            }    
         },
         Err(err) => {
             eprintln!("Error: {}", err);
